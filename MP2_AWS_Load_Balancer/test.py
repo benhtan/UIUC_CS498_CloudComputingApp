f9346c9b9c2b7aa94d791f5b1a8400274501480a
benhtan <btan90@gmail.com>
2022-01-28 21:18:26 -0800

    final submission

--- MP2_AWS_Load_Balancer/test.py
+++ MP2_AWS_Load_Balancer/test.py
@@ -4,11 +4,11 @@ import json
 url = 'https://seorwrpmwh.execute-api.us-east-1.amazonaws.com/prod/mp2-autograder-2022-spring'
 
 payload = {
-		'ip_address1':  # <insert ip address:port of first EC2 instance>, 
-		'ip_address2':  # <insert ip address:port of second EC2 instance>,
-		'load_balancer' :  # <insert address of load balancer>,
-		'submitterEmail':  # <insert your coursera account email>,
-		'secret':  # <insert your secret token from coursera>
+		'ip_address1': '54.183.95.202:5000', # <insert ip address:port of first EC2 instance>, 
+		'ip_address2': '3.101.13.19:5000', # <insert ip address:port of second EC2 instance>,
+		'load_balancer' : 'CS498-MP2-1274606434.us-west-1.elb.amazonaws.com', # <insert address of load balancer>,
+		'submitterEmail': 'btan90@gmail.com', # <insert your coursera account email>,
+		'secret': 'FfIzPkJMyOpbwGIq' # <insert your secret token from coursera>
 		}
 
 r = requests.post(url, data=json.dumps(payload))
