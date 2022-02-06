6fee53bf28d465a3185a9eb8e8b4893f8b274f29
benhtan <btan90@gmail.com>
2022-02-05 22:57:11 -0800

    incorporate with lambda

--- MP3_AWS_Lex_and_Lamda/lambda_function_distance.py
+++ MP3_AWS_Lex_and_Lamda/lambda_function_distance.py
@@ -1,3 +1,45 @@
+import json
+
+def lambda_handler(event, context):
+    graph = {"graph": "Chicago->Urbana,Urbana->Springfield,Chicago->Lafayette,Lafayette->XX,XX->Springfield"}
+
+    # split edges from string
+    edges = graph['graph'].split(',')
+    vertex = set()  # to store unique vertex (city)
+    distances = {}  # to store distances between vertex (city)
+    
+    # enter edges with length 1 to dict distances
+    for edge in edges:
+        cities = edge.split('->')   # get just the cities
+        distances[cities[0] + '->' + cities[1]] = 1  # enter the distance between to edge
+        distances[cities[1] + '->' + cities[0]] = 1  # enter the distance between to edge, the reverse
+        vertex.update(cities)   # enter unique cities to set
+    
+    vertex = list(vertex)   # convert set to list
+    for i in range(len(vertex)):
+        for j in range(i, len(vertex)):
+            # if origin and destination is the same city
+            if vertex[i] == vertex[j]:
+                distances[vertex[i] + '->' + vertex[j]] = 0
+            elif vertex[i]+vertex[j] not in distances:
+                pass
+                dist = calc_distance(vertex[i], vertex[j], distances)
+                distances[vertex[i] + '->' + vertex[j]] = dist
+                distances[vertex[j] + '->' + vertex[i]] = dist
+                # print(f'{vertex[i]}->{vertex[j]}: {dist}')
+        
+    
+    # print(edges)
+    # print(vertex)
+    print(distances)
+    # find_neighbour('Chicago', distances)
+    # print(calc_distance('Springfield', 'Lafayette', distances))
+
+    return {
+        'statusCode': 200,
+        'body': 'Success'
+    }
+    
 def calc_distance(origin, dest, distances):
     neighbour_q = [origin]  # queue
     visited = []    # tracking visited
@@ -36,37 +78,3 @@ def find_neighbour(city, distances):
         neighbour.append(edge[edge.index('->')+2:])
     # print(neighbour)
     return neighbour
-
-graph = {"graph": "Chicago->Urbana,Urbana->Springfield,Chicago->Lafayette,Lafayette->XX,XX->Springfield"}
-
-# split edges from string
-edges = graph['graph'].split(',')
-vertex = set()  # to store unique vertex (city)
-distances = {}  # to store distances between vertex (city)
-
-# enter edges with length 1 to dict distances
-for edge in edges:
-    cities = edge.split('->')   # get just the cities
-    distances[cities[0] + '->' + cities[1]] = 1  # enter the distance between to edge
-    distances[cities[1] + '->' + cities[0]] = 1  # enter the distance between to edge, the reverse
-    vertex.update(cities)   # enter unique cities to set
-
-vertex = list(vertex)   # convert set to list
-for i in range(len(vertex)):
-    for j in range(i, len(vertex)):
-        # if origin and destination is the same city
-        if vertex[i] == vertex[j]:
-            distances[vertex[i] + '->' + vertex[j]] = 0
-        elif vertex[i]+vertex[j] not in distances:
-            pass
-            dist = calc_distance(vertex[i], vertex[j], distances)
-            distances[vertex[i] + '->' + vertex[j]] = dist
-            distances[vertex[j] + '->' + vertex[i]] = dist
-            # print(f'{vertex[i]}->{vertex[j]}: {dist}')
-    
-
-# print(edges)
-# print(vertex)
-print(distances)
-# find_neighbour('Chicago', distances)
-# print(calc_distance('Springfield', 'Lafayette', distances))
