039faca8c49e11687d7180ee753dbd566f0088dc
benhtan <btan90@gmail.com>
2022-02-05 19:28:49 -0800

    add MP3

---
+++ MP3_AWS_Lex_and_Lamda/test.py
@@ -0,0 +1,21 @@
+import requests
+import json
+import uuid
+
+url = "https://seorwrpmwh.execute-api.us-east-1.amazonaws.com/prod/mp3-autograder-2022-spring"
+
+payload = {
+	"graphApi": #<post api for storing the graph>,
+	"botName": # <name of your Amazon Lex Bot>, 
+	"botAlias": # <alias name given when publishing the bot>,
+	"identityPoolId": #<cognito identity pool id for lex>,
+	"accountId": #<your aws account id used for accessing lex>,
+	"submitterEmail": # <insert your coursera account email>,
+	"secret": # <insert your secret token from coursera>,
+	"region": #<Region where your lex is deployed (Ex: us-east-1)>
+    }
+
+r = requests.post(url, data=json.dumps(payload))
+
+print(r.status_code, r.reason)
+print(r.text)
