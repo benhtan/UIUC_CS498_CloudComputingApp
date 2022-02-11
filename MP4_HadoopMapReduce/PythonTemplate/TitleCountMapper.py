e2ed93458b5546ffbd977304ec4d80c58f8f0574
benhtan <btan90@gmail.com>
2022-02-10 21:39:59 -0800

    titlecount mapper and reduce seems to be working

--- MP4_HadoopMapReduce/PythonTemplate/TitleCountMapper.py
+++ MP4_HadoopMapReduce/PythonTemplate/TitleCountMapper.py
@@ -7,8 +7,8 @@ import string
 
 # stopWordsPath = sys.argv[1]
 # delimitersPath = sys.argv[2]
-stopWordsPath = 'stopwords.txt'
-delimitersPath = 'delimiters.txt'
+stopWordsPath = 'stopwords.txt'     # comment before submit
+delimitersPath = 'delimiters.txt'   # comment before submit
 
 stopWords = []
 delimiters = []
@@ -17,28 +17,30 @@ delimiters = []
 with open(stopWordsPath) as f:
     # TODO
     stopWords = [line.rstrip('\n') for line in f]
-    # print(stopWords)
+    # print(stopWords)  # comment before submit
 
 # TODO 
 with open(delimitersPath) as f:
     # TODO
     delimiters = f.read().rstrip('\n')
     delimiters = [c for c in delimiters]
-    # print(delimiters)
+    # print(delimiters) # comment before submit
 
 # text = sys.stdin
-text = []
-textPath = 'titles-a.txt'
+text = []               # comment before submit
+textPath = 'input.txt'  # comment before submit
 
 def replaceDelimitersWithSpace(w, d):
     for e in d:
         w = w.replace(e, ' ')
     return w
 
-# print(replaceDelimitersWithSpace('Benson_&_Hedges_Match_Play_Championship',delimiters))
+# print(replaceDelimitersWithSpace('Benson_&_Hedges_Match_Play_Championship',delimiters))   # comment before submit
 
-with open(textPath, encoding="utf8") as f:
-    text = [line.rstrip('\n') for line in f]
+with open(textPath, encoding="utf8") as f:      # comment before submit
+    text = [line.rstrip('\n') for line in f]    # comment before submit
+
+f = open('outputText.txt', 'w', encoding="utf8")    # comment before submit
 
 for line in text:
       
@@ -47,6 +49,10 @@ for line in text:
     
     for word in word_list:
         if word not in stopWords:
-            print(word.lower() + '\t' + '1')
+            output = word.lower() + '\t' + '1'
+            # print(output)             # needed for submission
+            f.write(output + '\n')      # comment before submit
 
     # print('%s\t%s' % (  ,  )) pass this output to reducer
+    
+f.close()   # comment before submit
--- MP4_HadoopMapReduce/PythonTemplate/TitleCountReducer.py
+++ MP4_HadoopMapReduce/PythonTemplate/TitleCountReducer.py
@@ -3,10 +3,33 @@ from operator import itemgetter
 import sys
 
 #TODO
+# text = sys.stdin
+text = []
+textPath = 'outputText.txt'
+with open(textPath, encoding="utf8") as f:
+    text = [line.rstrip('\n') for line in f]
 
+word_counter = {}
 # input comes from STDIN
-for line in sys.stdin:
+for line in text:
     # TODO
+    word_count = line.split('\t')
+    
+    if word_count[0] in word_counter:
+        word_counter[word_count[0]] += int(word_count[1])
+    else:
+        word_counter[word_count[0]] = 1
+
+# print(word_counter)
+f = open('wordCount.txt', 'w', encoding="utf8")
+
+for word in word_counter:
+    output = f'{word}\t{word_counter[word]}'
+    # print(output)
+    f.write(output + '\n')
+    
 
 # TODO
 # print('%s\t%s' % (  ,  )) print as final output
+
+f.close()
