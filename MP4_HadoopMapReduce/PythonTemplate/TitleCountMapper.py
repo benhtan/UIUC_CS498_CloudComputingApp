267f996eaf701367586deca8a3be17d782586c21
benhtan <btan90@gmail.com>
2022-02-10 20:20:02 -0800

    parse delimiter and stop words

--- MP4_HadoopMapReduce/PythonTemplate/TitleCountMapper.py
+++ MP4_HadoopMapReduce/PythonTemplate/TitleCountMapper.py
@@ -5,27 +5,32 @@ import string
 
 
 
-stopWordsPath = sys.argv[1]
-delimitersPath = sys.argv[2]
+# stopWordsPath = sys.argv[1]
+# delimitersPath = sys.argv[2]
+stopWordsPath = 'stopwords.txt'
+delimitersPath = 'delimiters.txt'
 
+stopWords = []
+delimiters = []
 
 # TODO
 with open(stopWordsPath) as f:
     # TODO
+    stopWords = [line.rstrip('\n') for line in f]
+    # print(stopWords)
 
-
-
-
-
-
-#TODO 
+# TODO 
 with open(delimitersPath) as f:
     # TODO
+    delimiters = f.read().rstrip('\n')
+    delimiters = [c for c in delimiters]
+    # print(delimiters)
 
-for line in sys.stdin:
+# for line in sys.stdin:
   
-    # TODO
+#     # TODO
+#     print(line)
 
-    # print('%s\t%s' % (  ,  )) pass this output to reducer
+#     # print('%s\t%s' % (  ,  )) pass this output to reducer
 
 
