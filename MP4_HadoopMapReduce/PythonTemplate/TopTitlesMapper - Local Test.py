cf26be76d508acd766808b118e1681e57e5725cd
benhtan <btan90@gmail.com>
2022-02-11 20:55:01 -0800

    top titles mapper working

--- MP4_HadoopMapReduce/PythonTemplate/TopTitlesMapper - Local Test.py
+++ MP4_HadoopMapReduce/PythonTemplate/TopTitlesMapper - Local Test.py
@@ -5,6 +5,7 @@ import sys
 # TODO
 
 f = open('prePartA_output_TitleCount.txt', encoding="utf8")
+n = open('partA_top5_mapper_output.txt', 'w', encoding="utf8")
 words = []
 
 for line in f:
@@ -13,8 +14,13 @@ for line in f:
 
 words.sort(key=lambda t: int(t[1]))
 
-print(words[-5:])
+# print(words[-5:])
 #TODO
+for word in words[-5:]:
+       output = f'{word[0]}\t{word[1]}'
+       print(output)
+       n.write(output + '\n')
 # print('%s\t%s' % (  ,  )) pass this output to reducer
 
 f.close()
+n.close()
