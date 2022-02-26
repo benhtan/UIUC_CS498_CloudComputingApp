29bacdf46f413ac523377e4407035cc86c1830d5
benhtan <btan90@gmail.com>
2022-02-26 10:25:53 -0800

    toppopularlinks initial

--- MP5_SparkMapReduce/PythonTemplate/TopPopularLinksSpark.py
+++ MP5_SparkMapReduce/PythonTemplate/TopPopularLinksSpark.py
@@ -9,11 +9,30 @@ sc = SparkContext(conf=conf)
 lines = sc.textFile(sys.argv[1], 1) 
 
 #TODO
+def parentChild(line):
+    line = line.rstrip('\n').split(': ')
+    parent = line[0]
+    childern = line[1].split()
+    # childern = [int(e) for e in childern]   # convert to int
+    # childern = [child for child in childern if child != parent]     # remove self link pages
+    
+    res = [(parent, 0)]
+    
+    for child in childern:
+        res.append((child,1))
+        
+    return res
+
+lines = lines.flatMap(lambda line: parentChild(line)).reduceByKey(lambda a, b: a + b)
+lines = lines.take(500)
 
 output = open(sys.argv[2], "w")
 
 #TODO
 #write results to output file. Foramt for each line: (key + \t + value +"\n")
+
+for line in lines:
+    output.write(f'{line}\n')
 
 sc.stop()
 
