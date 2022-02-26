c9060575e065228e7d9308ab0461f8f20afb7802
benhtan <btan90@gmail.com>
2022-02-26 09:36:20 -0800

    starting orphanpages

--- MP5_SparkMapReduce/PythonTemplate/OrphanPagesSpark.py
+++ MP5_SparkMapReduce/PythonTemplate/OrphanPagesSpark.py
@@ -9,11 +9,28 @@ sc = SparkContext(conf=conf)
 lines = sc.textFile(sys.argv[1], 1) 
 
 #TODO
+def parentChild(line):
+    line = line.rstrip('\n').split(': ')
+    parent = int(line[0])
+    childern = line[1].split()
+    childern = [int(e) for e in childern]
+    
+    res = [(parent, 0)]
+    
+    for child in childern:
+        res.append((child,1))
+        
+    return res
+
+lines = lines.flatMap(lambda line: parentChild(line))
+lines = lines.take(500)
 
 output = open(sys.argv[2], "w")
 
 #TODO
 #write results to output file. Foramt for each line: (line + "\n")
+for line in lines:
+    output.write(f'line\n')
 
 sc.stop()
 
