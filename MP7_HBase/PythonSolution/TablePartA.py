31ec2dd4202f2e0d0aff0237280460a39ec53d3e
benhtan <btan90@gmail.com>
2022-03-17 20:15:09 -0700

    working on part B

--- MP7_HBase/PythonSolution/TablePartA.py
+++ MP7_HBase/PythonSolution/TablePartA.py
@@ -1,1 +1,28 @@
 import happybase as hb
+
+# Make connection
+connection = hb.Connection()
+
+# Delete prior created tables
+tables = connection.tables()
+for t in tables:
+    connection.delete_table(t, disable=True)
+
+# Set families for columns
+powers_families = {
+    'personal': {},
+    'professional': {},
+    'custom': {},
+}
+
+food_families = {
+    'nutrition': {},
+    'taste': {},
+}
+
+# Make tables
+connection.create_table('powers', powers_families)
+connection.create_table('food', food_families)
+
+# Close Connection
+connection.close()
--- MP7_HBase/PythonSolution/TablePartB.py
+++ MP7_HBase/PythonSolution/TablePartB.py
@@ -1,2 +1,10 @@
 import happybase as hb
 
+# Make connection
+connection = hb.Connection()
+
+tables = connection.tables()
+print(tables)
+
+# Close Connection
+connection.close()
