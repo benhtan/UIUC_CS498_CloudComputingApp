347f873999c923864e4f4a322853f756ada7a194
benhtan <btan90@gmail.com>
2022-03-17 20:46:59 -0700

    working on partC

--- MP7_HBase/PythonSolution/TablePartC.py
+++ MP7_HBase/PythonSolution/TablePartC.py
@@ -1,3 +1,19 @@
 import happybase as hb
 import csv
 
+# Make connection
+connection = hb.Connection()
+powers_table = connection.table('powers')
+
+file = open('input.csv', 'r')
+reader = csv.reader(file) # List of list ['row1', 'yes', 'fly', 'batman', '100', 'black']
+
+batch = powers_table.batch()
+for row in reader:
+    batch.put( row[0], {'personal:hero': row[1], 'personal:power': row[2], 'professional:name': row[3], 'professional:xp': row[4], 'custom:color': row[5]}  )
+    
+batch.send()
+
+# Close Connection
+connection.close()
+file.close()
