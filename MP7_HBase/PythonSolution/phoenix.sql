71f2430a2052b2944bd5d3823935ea50b962d15b
benhtan <btan90@gmail.com>
2022-03-19 09:00:56 -0700

    change column name

--- MP7_HBase/PythonSolution/phoenix.sql
+++ MP7_HBase/PythonSolution/phoenix.sql
@@ -1,6 +1,6 @@
 DROP VIEW IF EXISTS "powers";
 CREATE VIEW "powers" ( pk VARCHAR PRIMARY KEY, "professional"."name" VARCHAR,"personal"."power" VARCHAR, "personal"."hero" VARCHAR );
-SELECT *
+SELECT p."professional"."name" as "Name1", p1."professional"."name" as "Name2", p."personal"."power" as "Power"
 FROM "powers" AS p
 INNER JOIN "powers" as p1
 ON p."personal"."power" = p1."personal"."power"
