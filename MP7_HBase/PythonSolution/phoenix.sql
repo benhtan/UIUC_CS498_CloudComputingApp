ddb25916a80a6cab3b2c5aab0ed0d86b6cf7dd04
benhtan <btan90@gmail.com>
2022-03-18 21:11:21 -0700

    working on parG

--- MP7_HBase/PythonSolution/phoenix.sql
+++ MP7_HBase/PythonSolution/phoenix.sql
@@ -1,1 +1,4 @@
-
+DROP TABLE powers;
+CREATE VIEW "powers" ( pk VARCHAR PRIMARY KEY, "personal".val VARCHAR, "professional".val VARCHAR, "custom".val VARCHAR);
+SELECT * FROM powers;
+-- WHERE p1."hero" = 'yes' AND p2."hero" = 'yes';
