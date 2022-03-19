e5d59a617d8b5dcef5c97d4b1ac043bd40c53688
benhtan <btan90@gmail.com>
2022-03-18 19:54:47 -0700

    partF seems to work

--- MP7_HBase/PythonSolution/TablePartF.py
+++ MP7_HBase/PythonSolution/TablePartF.py
@@ -12,24 +12,24 @@ powers_scan = []
 for key, data in powers_table.scan():
     powers_scan.append(data)
 
-print(powers_scan)
+# print(powers_scan)
 
-# for key, data in powers_scan:
-#     # print(f'data: {data}')
-#     for key1, data1 in powers_scan1:
-#         color = data[b'custom:color']
-#         name = data[b'professional:name']
-#         power = data[b'personal:power']
+for data in powers_scan:
+    # print(f'data: {data}')
+    for data1 in powers_scan:
+        color = data[b'custom:color']
+        name = data[b'professional:name']
+        power = data[b'personal:power']
 
-#         color1 = data1[b'custom:color']
-#         name1 = data1[b'professional:name']
-#         power1 = data1[b'personal:power']
+        color1 = data1[b'custom:color']
+        name1 = data1[b'professional:name']
+        power1 = data1[b'personal:power']
         
         # print(f'data1: {data1}')
         # print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
         
-        # if color == color1 and name != name1:
-        #     print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
+        if color == color1 and name != name1:
+            print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
 
 # Close Connection
 connection.close()
