38d31b49f35185b4db2c0eb6a4f8c5f4afaa9c6e
benhtan <btan90@gmail.com>
2022-03-18 19:52:28 -0700

    debugging partF

--- MP7_HBase/PythonSolution/TablePartF.py
+++ MP7_HBase/PythonSolution/TablePartF.py
@@ -7,18 +7,29 @@ import happybase as hb
 connection = hb.Connection()
 powers_table = connection.table('powers')
 
-for key, data in powers_table.scan():
-    for key1, data1 in powers_table.scan():
-        color = data[b'custom:color']
-        name = data[b'professional:name']
-        power = data[b'personal:power']
+powers_scan = []
 
-        color1 = data1[b'custom:color']
-        name1 = data1[b'professional:name']
-        power1 = data1[b'personal:power']
+for key, data in powers_table.scan():
+    powers_scan.append(data)
+
+print(powers_scan)
+
+# for key, data in powers_scan:
+#     # print(f'data: {data}')
+#     for key1, data1 in powers_scan1:
+#         color = data[b'custom:color']
+#         name = data[b'professional:name']
+#         power = data[b'personal:power']
+
+#         color1 = data1[b'custom:color']
+#         name1 = data1[b'professional:name']
+#         power1 = data1[b'personal:power']
         
-        if color == color1 and name != name1:
-            print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
+        # print(f'data1: {data1}')
+        # print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
+        
+        # if color == color1 and name != name1:
+        #     print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
 
 # Close Connection
 connection.close()
