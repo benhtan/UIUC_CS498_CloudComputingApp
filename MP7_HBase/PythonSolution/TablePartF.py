6133a8ff0a32632bacf14a13afe0303f5531c538
benhtan <btan90@gmail.com>
2022-03-18 19:29:07 -0700

    working on partF

--- MP7_HBase/PythonSolution/TablePartF.py
+++ MP7_HBase/PythonSolution/TablePartF.py
@@ -3,14 +3,22 @@ import happybase as hb
 # DON'T CHANGE THE PRINT FORMAT, WHICH IS THE OUTPUT
 # OR YOU WON'T RECEIVE POINTS FROM THE GRADER
 
-color = ???
-name = ???
-power = ???
+# Make connection
+connection = hb.Connection()
+powers_table = connection.table('powers')
 
-color1 = ???
-name1 = ???
-power1 = ???
+for key, data in powers_table.scan():
+    for key1, data1 in powers_table.scan():
+        color = data[b'custom:color']
+        name = data[b'professional:name']
+        power = data[b'personal:power']
 
-print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
+        color1 = data1[b'custom:color']
+        name1 = data1[b'professional:name']
+        power1 = data1[b'personal:power']
+        
+        if color == color1 and name != name1:
+            print('{}, {}, {}, {}, {}'.format(name, power, name1, power1, color))
 
-
+# Close Connection
+connection.close()
