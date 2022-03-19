451cecea5f907e2401fc815c49e9de6dae63202e
benhtan <btan90@gmail.com>
2022-03-19 08:22:55 -0700

    debugging sql

--- MP7_HBase/PythonSolution/phoenix.sql
+++ MP7_HBase/PythonSolution/phoenix.sql
@@ -1,4 +1,9 @@
-DROP TABLE powers;
+DROP VIEW "powers";
 CREATE VIEW "powers" ( pk VARCHAR PRIMARY KEY, "personal".val VARCHAR, "professional".val VARCHAR, "custom".val VARCHAR);
-SELECT * FROM powers;
+SELECT p.name, p.power
+-- FROM "powers"
+FROM "powers" AS p
+-- INNER JOIN powers as p1
+-- ON p.power = p1.power
+;
 -- WHERE p1."hero" = 'yes' AND p2."hero" = 'yes';
