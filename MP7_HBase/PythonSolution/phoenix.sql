8da50878ecedd21b9c862e0e851b4367f5b46e02
benhtan <btan90@gmail.com>
2022-03-19 11:06:15 -0700

    pass autograder

--- MP7_HBase/PythonSolution/phoenix.sql
+++ MP7_HBase/PythonSolution/phoenix.sql
@@ -1,4 +1,4 @@
-DROP VIEW IF EXISTS "powers";
+-- DROP VIEW IF EXISTS "powers";
 CREATE VIEW "powers" ( pk VARCHAR PRIMARY KEY, "professional"."name" VARCHAR,"personal"."power" VARCHAR, "personal"."hero" VARCHAR );
 SELECT p."professional"."name" as "Name1", p1."professional"."name" as "Name2", p."personal"."power" as "Power"
 FROM "powers" AS p
