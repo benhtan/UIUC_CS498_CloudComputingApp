149fcf5259cef19fb0ade90120209479c99f674f
benhtan <btan90@gmail.com>
2022-03-20 15:47:45 -0700

    saving progress on partA

--- MP8_SparkSQL/pythonSolution/MP8_PartA.py
+++ MP8_SparkSQL/pythonSolution/MP8_PartA.py
@@ -2,9 +2,17 @@ from pyspark import SparkContext, SQLContext
 from pyspark.sql.types import StructType
 from pyspark.sql.types import StructField
 from pyspark.sql.types import StringType, IntegerType
+from pyspark.sql import SparkSession
 
 sc = SparkContext()
 sqlContext = SQLContext(sc)
+spark = SparkSession(sc)
+
+gbooks = sc.textFile("gbooks").map(lambda t: t.split())
+# print(gbooks.collect())
+
+gbooks_df = spark.createDataFrame(gbooks)
+print(gbooks_df.collect())
 
 ####
 # 1. Setup (10 points): Download the gbook file and write a function to load it in an RDD & DataFrame
