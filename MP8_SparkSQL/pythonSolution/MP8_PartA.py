beaf45365b73c9fc75abb5dc7d724d44839cbbb8
benhtan <btan90@gmail.com>
2022-03-20 16:19:44 -0700

    add comments. pass autograder

--- MP8_SparkSQL/pythonSolution/MP8_PartA.py
+++ MP8_SparkSQL/pythonSolution/MP8_PartA.py
@@ -8,14 +8,18 @@ sc = SparkContext()
 sqlContext = SQLContext(sc)
 spark = SparkSession(sc)
 
+# read from text file. split each line into 4 colums. convert to tuple (string, int, int, int)
 gbooks = sc.textFile("gbooks").map(lambda t: t.split()).map( lambda t: ( t[0], int(t[1]), int(t[2]), int(t[3]) ) )
 # print(gbooks.collect())
 
+# create dataframe schema
 schema = StructType([ StructField("word", StringType()), StructField("count1", IntegerType()), StructField("count2", IntegerType()), StructField("count3", IntegerType()) ])
 
+# create dataframe using data and schema
 gbooks_df = spark.createDataFrame(gbooks, schema)
 # print(gbooks_df.collect())
 
+# printSchema
 gbooks_df.printSchema()
 
 ####
