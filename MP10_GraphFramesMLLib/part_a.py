c3de5b79cb936cd50e3c37f339d038639b90a854
benhtan <btan90@gmail.com>
2022-04-02 12:55:24 -0700

    working on partA grouping components

--- MP10_GraphFramesMLLib/part_a.py
+++ MP10_GraphFramesMLLib/part_a.py
@@ -13,6 +13,15 @@ def get_connected_components(graphframe):
     # For example, [[a_1, a_2, ...], [b_1, b_2, ...], ...]
     # then a_1, a_2, ..., a_n lie in the same component,
     # b_1, b2, ..., b_m lie in the same component, etc
+    comp = g.connectedComponents().collect()
+    result_dict = {}
+    for row in comp:
+        if row['component'] not in result_dict:
+            result_dict[row['component']] = [row['id']]
+        else:
+            result_dict[row['component']].append(row['id'])
+    print(result_dict)
+    # print(result.select("id", "component").orderBy("component").collect())
     return [[]]
 
 
@@ -32,9 +41,9 @@ if __name__ == "__main__":
     vertices = spark.createDataFrame(vertex_list, ["id"])  # TODO: Create vertices dataframe
     edges = spark.createDataFrame(edge_list, ["src", "dst"])  # TODO: Create edges dataframe
 
-    # g = GraphFrame(vertices, edges)
-    # sc.setCheckpointDir("/tmp/connected-components")
+    g = GraphFrame(vertices, edges)
+    sc.setCheckpointDir("/tmp/connected-components")
 
-    # result = get_connected_components(g)
+    result = get_connected_components(g)
     # for line in result:
     #     print(' '.join(line))
