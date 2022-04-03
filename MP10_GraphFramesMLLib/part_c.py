6946bf37b9cd76e0198391fb9f6afda39898a344
benhtan <btan90@gmail.com>
2022-04-03 16:05:21 -0700

    starting partC

--- MP10_GraphFramesMLLib/part_c.py
+++ MP10_GraphFramesMLLib/part_c.py
@@ -11,6 +11,8 @@ def get_shortest_distances(graphframe, dst_id):
     # Find shortest distances in the given graphframe to the vertex which has id `dst_id`
     # The result is a dictionary where key is a vertex id and the corresponding value is
     # the distance of this node to vertex `dst_id`.
+    res = graphframe.shortestPaths(landmarks=dst_id)
+    res.show()
     return {}
 
 
@@ -20,14 +22,14 @@ if __name__ == "__main__":
     with open('dataset/graph.data') as f:
         for line in f:
             # TODO: Parse line to get vertex id
-            src = None
+            src = line.split()[0]
             # TODO: Parse line to get ids of vertices that src is connected to
-            dst_list = []
+            dst_list = line.split()[1:]
             vertex_list.append((src,))
             edge_list += [(src, dst) for dst in dst_list]
 
-    vertices = spark.createDataFrame(...)  # TODO: Create dataframe for vertices
-    edges = spark.createDataFrame(...)  # TODO: Create dataframe for edges
+    vertices = spark.createDataFrame(vertex_list, ["id"])  # TODO: Create dataframe for vertices
+    edges = spark.createDataFrame(edge_list, ["src", "dst"])  # TODO: Create dataframe for edges
 
     g = GraphFrame(vertices, edges)
     sc.setCheckpointDir("/tmp/shortest-paths")
