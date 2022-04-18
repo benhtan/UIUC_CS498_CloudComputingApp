441684b761436ee80679ad822496270024bfc8e7
benhtan <btan90@gmail.com>
2022-04-17 20:40:04 -0700

    saving progress on part D

--- MP11_Storm/PythonSolution/multilang/resources/top_n_finder_bolt.py
+++ MP11_Storm/PythonSolution/multilang/resources/top_n_finder_bolt.py
@@ -14,10 +14,12 @@ class TopNFinderBolt(storm.BasicBolt):
 
         # TODO:
         # Task: set N
-        pass
+        self._N = conf.get('N')    # a constant
         # End
 
         # Hint: Add necessary instance variables and classes if needed
+        self._topN = []         # list of tuples
+        self._wordCount = {}    # dict of word and the count
 
     def process(self, tup):
         '''
@@ -26,7 +28,14 @@ class TopNFinderBolt(storm.BasicBolt):
         Hint: implement efficient algorithm so that it won't be shutdown before task finished
               the algorithm we used when we developed the auto-grader is maintaining a N size min-heap
         '''
-        pass
+        self._wordCount[tup.values[0]] = tup.values[1]
+        counter = Counter(self._wordCount)
+        topN = counter.most_common(self._N)     # list of tuples
+        
+        if topN != self._topN:
+            self._topN = topN
+            tempList = [e[0] for e in self._topN]
+            storm.emit( [ f'top-{self._N}', ', '.join(tempList) ] )
         # End
 
 
