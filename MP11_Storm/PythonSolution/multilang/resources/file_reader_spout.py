5c8e47f73586626e8f6b6c0338bfb0d36fe64945
benhtan <btan90@gmail.com>
2022-04-17 18:09:00 -0700

    parallelism needs to be 1

--- MP11_Storm/PythonSolution/multilang/resources/file_reader_spout.py
+++ MP11_Storm/PythonSolution/multilang/resources/file_reader_spout.py
@@ -18,14 +18,19 @@ class FileReaderSpout(storm.Spout):
         # TODO:
         # Task: Initialize the file reader
         # hint: get the filename from conf argument 
-        pass
+        filePath = conf.get("filePath")
+        self._f = open(filePath, "r")
         # End
 
     def nextTuple(self):
         # TODO:
         # Task 1: read the next line and emit a tuple for it
         # Task 2: don't forget to sleep for 1 second when the file is entirely read to prevent a busy-loop
-        pass
+        for sentence in self._f.readlines():
+            storm.logInfo("Emiting %s" % sentence)
+            storm.emit([sentence])
+            
+        sleep(1)
         # End
 
 
