cba7be5248c97f6f61567db3e1babd277c44a7e5
benhtan <btan90@gmail.com>
2022-04-29 16:14:54 -0700

    server.py seems to be working

--- MP12_Virtualization/server.py
+++ MP12_Virtualization/server.py
@@ -35,6 +35,17 @@ def get_config():
 @app.route('/img-classification/free',methods=['POST'])
 def post_free():
     # your code here
+    with open(path.join(path.dirname(__file__), "free-job.yaml")) as f:
+        # Look into the usage of yaml.safe_load()
+        dep = yaml.safe_load(f)
+        # print `dep` to see the structure
+
+        # To-do: generate a random deployId
+        # To-do: replace some attributes of `dep` with the arguments in `body`
+
+        k8s_apps_v1 = client.BatchV1Api()
+        resp = k8s_apps_v1.create_namespaced_job(body=dep, namespace="free-service")
+        # print `resp` to see the response
 
     return "success"
 
@@ -42,6 +53,17 @@ def post_free():
 @app.route('/img-classification/premium', methods=['POST'])
 def post_premium():
     # your code here
+    with open(path.join(path.dirname(__file__), "premium-job.yaml")) as f:
+        # Look into the usage of yaml.safe_load()
+        dep = yaml.safe_load(f)
+        # print `dep` to see the structure
+
+        # To-do: generate a random deployId
+        # To-do: replace some attributes of `dep` with the arguments in `body`
+
+        k8s_apps_v1 = client.BatchV1Api()
+        resp = k8s_apps_v1.create_namespaced_job(body=dep, namespace="default")
+        # print `resp` to see the response
 
     return "success"
 
