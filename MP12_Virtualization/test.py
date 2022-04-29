e961d0701a379108eaf63f4859fc6fcce5824305
benhtan <btan90@gmail.com>
2022-04-29 16:20:06 -0700

    add git ignore

--- MP12_Virtualization/test.py
+++ MP12_Virtualization/test.py
@@ -3,7 +3,7 @@ import json
 url = "https://seorwrpmwh.execute-api.us-east-1.amazonaws.com/prod/mp12-grader"
 payload = {
             "accountId": 123455100654,
-            "submitterEmail": '12345@illinois.edu',
+            "submitterEmail": 'asdads@gmail.com',
             "secret": 'FVPlDlckg68Jwhei',
             "ipaddress": '3.235.141.208:5000'
     }
